8b501e3ac4a286407dfcbc17c371c820d6c98af2
Vazgen <Vazgen2004proweb@gmail.com>
2024-09-29 19:05:12 +0200

    Init commit

---
+++ src/test/java/cz/cvut/omo/smart_farm/SmartFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.omo.smart_farm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
