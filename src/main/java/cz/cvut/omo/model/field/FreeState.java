e81a6cd75acfeedc952e0fbcad9c79562290bae7
Vitalii Stepaniuk <stepavi2@fel.cvut.cz>
2024-12-02 14:42:20 +0100

    state pattern implementation in Field Class

---
+++ src/main/java/cz/cvut/omo/model/field/FreeState.java
@@ -0,0 +1,19 @@
+package cz.cvut.omo.model.field;
+
+public class FreeState implements FieldState {
+    @Override
+    public void plant(Field field) {
+        field.setState(new PlantedState());
+        System.out.println("The field has been planted.");
+    }
+
+    @Override
+    public void harvest(Field field) {
+        System.out.println("The field is free. Nothing to harvest.");
+    }
+
+    @Override
+    public void applyPesticides(Field field) {
+        System.out.println("The field is free. No need for pesticides.");
+    }
+}
