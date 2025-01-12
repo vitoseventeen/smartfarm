d57ae33e5f1e729b60e8ddbc284e8f2f4db232e7
Vitalii Stepaniuk <stepavi2@fel.cvut.cz>
2025-01-12 13:43:58 +0100

    javadoc comments added

--- src/main/java/cz/cvut/fel/omo/smartfarm/chainOfResponsibility/EventHandler.java
+++ src/main/java/cz/cvut/fel/omo/smartfarm/chainOfResponsibility/EventHandler.java
@@ -1,13 +1,26 @@
 package cz.cvut.fel.omo.smartfarm.chainOfResponsibility;
 
+/**
+ * Abstract class for handling events in a chain of responsibility pattern.
+ * This class provides the basic framework for linking handlers and passing events along the chain.
+ */
 public abstract class EventHandler {
     protected EventHandler nextHandler;
 
+    /**
+     * Sets the next handler in the chain of responsibility.
+     *
+     * @param nextHandler The next EventHandler to set in the chain.
+     */
     public void setNextHandler(EventHandler nextHandler) {
         this.nextHandler = nextHandler;
     }
 
+    /**
+     * Handles an event. Concrete implementations of this method will define how specific types of events are handled.
+     * If the event cannot be handled, it should be passed to the next handler in the chain, if one exists.
+     *
+     * @param event The event to be handled.
+     */
     public abstract void handleEvent(Event event);
-
-
 }
